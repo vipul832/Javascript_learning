0376e91c3328bd80ebb54f9ddbd71359da38298a
vipulc8 <vipul.c@simformsolutions.com>
2023-01-24 13:55:37 +0530

    Adding the item

--- app.js
+++ app.js
@@ -1,74 +1,11 @@
+//Local Storage
 
-//Functions
+localStorage.setItem("name", "Vipul"); //it will store in local storage
 
-//Functions Statements  this also called as function Declaration
+//to get value from the local storage use "getItem"
 
-function name() {}
+console.log(localStorage.getItem("name"));
 
-//Fucntion Expression
+// //to remove item from local storage
 
-var c = function () {
-  console.log("Function Expression");
-};
-c();
-
-// anonymous Function     //a function with out name and can pass as a value.
-
-// function ()
-// {
-
-// }
-
-//named function expression
-
-var a = function xyz() {
-  console.log("named function expression");
-};
-
-a();
-
-//First Class Function   // The abality to pass a function inside a function  and can return a function
-
-function test(re) {
-  re();
-}
-
-function resutl() {
-  console.log("Result");
-}
-
-test(resutl);
-
-//Arrow Function
-
-var sum = () => 10 + 15;
-
-var result = sum();
-console.log(result);
-
-//CallBack Function  are passed as an argument to another functions
-//callBack Function can run after another function has finished.
-
-function display2(total) {
-  console.log("result:", total);
-}
-
-function sum2(num1, num2, display2) {
-  let total = num1 + num2;
-  display2(total);
-}
-
-sum2(3, 32, display2); //passing a function as argument
-=======
-const pro = fetch("https://jsonplaceholder.typicode.com/users");
-
-console.log(pro); //It show promise
-
-//This show the promiseState and PromiseResults where our data is store
-data = pro
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+// localStorage.removeItem("name");
