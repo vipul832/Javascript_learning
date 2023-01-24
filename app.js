85aa2a3006c544065a7fcc9a958e25f5bdceafbc
vipulc8 <vipul.c@simformsolutions.com>
2023-01-24 13:09:28 +0530

    use closure

--- app.js
+++ app.js
@@ -1,74 +1,21 @@
+//Closure Function
 
-//Functions
+function cloth() {
+  let clo = "Shirt";
 
-//Functions Statements  this also called as function Declaration
+  //if you buy shirt you need pants right
+  function pant(pa) {
+    console.log(clo); //here it will take the latest value of variable in closure
+    return "With " + clo + " buy " + pa + " also.";
+  }
 
-function name() {}
-
-//Fucntion Expression
-
-var c = function () {
-  console.log("Function Expression");
-};
-c();
-
-// anonymous Function     //a function with out name and can pass as a value.
-
-// function ()
-// {
-
-// }
-
-//named function expression
-
-var a = function xyz() {
-  console.log("named function expression");
-};
-
-a();
-
-//First Class Function   // The abality to pass a function inside a function  and can return a function
-
-function test(re) {
-  re();
+  clo = "T-shirt";
+  return pant;
 }
 
-function resutl() {
-  console.log("Result");
-}
+let clo1 = cloth();
 
-test(resutl);
+//now i can use pant() out side it parent function
 
-//Arrow Function
-
-var sum = () => 10 + 15;
-
-var result = sum();
-console.log(result);
-
-//CallBack Function  are passed as an argument to another functions
-//callBack Function can run after another function has finished.
-
-function display2(total) {
-  console.log("result:", total);
-}
-
-function sum2(num1, num2, display2) {
-  let total = num1 + num2;
-  display2(total);
-}
-
-sum2(3, 32, display2); //passing a function as argument
-=======
-const pro = fetch("https://jsonplaceholder.typicode.com/users");
-
-console.log(pro); //It show promise
-
-//This show the promiseState and PromiseResults where our data is store
-data = pro
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+console.log(clo1("pant"));
+console.log(clo1);
