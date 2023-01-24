fbb2ac361e53e8125b09cd0ccda3913dc1c08ea8
vipulc8 <vipul.c@simformsolutions.com>
2023-01-24 13:55:37 +0530

    Local storage (cherry pick)

--- app.js
+++ app.js
@@ -19,3 +19,14 @@ let clo1 = cloth();
 
 console.log(clo1("pant"));
 console.log(clo1);
+//Local Storage
+
+localStorage.setItem("name", "Vipul"); //it will store in local storage
+
+//to get value from the local storage use "getItem"
+
+console.log(localStorage.getItem("name"));
+
+// //to remove item from local storage
+
+// localStorage.removeItem("name");
