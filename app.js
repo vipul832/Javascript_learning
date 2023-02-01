85f4d00d2953e8db08dbee0b64c8605122cbdb3b
vipulc8 <vipul.c@simformsolutions.com>
2023-02-01 12:07:22 +0530

    Default and Name Export

--- app.js
+++ app.js
@@ -1,32 +1,6 @@
-let car = {
-  name: "Audi",
-  model: "Q3",
-  speed() {
-    return "220 km\\h";
-  },
-  engine() {
-    return "working";
-  },
-};
+import Fruit, { sayHello } from "./modul1.js";
 
-let sportcar = {
-  name: "Audi",
-  model: "RS 7",
-  speed() {
-    return "250 km\\h";
-  },
-  exhaust() {
-    return "Loud Sound";
-  },
-};
+const apple = new Fruit("apple");
+apple.getType();
 
-// sportcar.__proto__ = car; //we are inherit the property from the car object. It not proper way to do.
-
-// console.log(sportcar.engine());
-// console.log(sportcar.speed());
-
-//better  way to write is as below
-
-let sportcar1 = Object.create(car); //__proto__ point to base prototype {}
-
-console.log(sportcar1.engine());
+sayHello();
