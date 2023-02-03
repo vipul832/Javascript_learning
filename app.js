04a663c9334ed67f0f1f38bb6cebddc76922855a
vipul chandravadiya <122255127+vipulc8@users.noreply.github.com>
2023-02-03 16:24:12 +0530

    Revert "Revert "Add pass by value and reference.""

--- app.js
+++ app.js
@@ -57,4 +57,57 @@ async function dataget() {
     });
   console.log("user", user);
 }
+
+sum2(3, 32, display2); 
+
+
 dataget();
+
+
+//passing a function as argument
+
+// pass by value
+
+function passbyvalue(a, b) {
+  //so in pass by value our actual value never change function creat new copy of argument then use in function
+  let temp;
+  temp = a;
+  a = b;
+  b = temp;
+
+  console.log(`value of a and b inside function ${a} ${b}`);
+}
+
+let a1 = 10;
+let b1 = 40;
+console.log(`Value of a and b before pass ${a1} ${b1}`);
+
+passbyvalue(a1, b1);
+
+console.log(`Value of a and b after pass ${a1} ${b1}`);
+
+// in pass by reference we use array and object
+
+function passbyref(obj) {
+  // as you se
+  obj.a = obj.a * 2;
+  obj.b = obj.b * 2;
+
+  console.log(`Value a and b inside pass by reference: ${obj.a} and ${obj.b}`);
+}
+
+let pobj = {
+  a: 12,
+  b: 0,
+};
+
+console.log(`Value a and b before pass by reference: ${pobj.a} and ${pobj.b}`);
+
+passbyref(pobj);
+
+console.log(`Value a and b after pass by reference: ${pobj.a} and ${pobj.b}`);
+
+
+
+
+
